b4b938d0813a56c025a9ffc293edd1f804366a91
rgEhrlick <li@realglobe.jp>
2012-07-10 17:51:38 +0900

    a

--- cakephp/app/controllers/posts_controller.php
+++ cakephp/app/controllers/posts_controller.php
@@ -3,7 +3,7 @@ class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
 
     public function index() {
-        $this->set('posts', $this->Post->find('all'));
+        $this->set('posts', $this->Post->findAll);
     }
 }
 ?>
